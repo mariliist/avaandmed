ebe7ca0d8fe00497a2cabc492a8864362f603213
Mariliis Toon <mariliistoon@gmail.com>
2024-02-22 16:06:17 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const selectHTMLElement = document.querySelector("#open-data");
+
+
+const baseURL = "https://avaandmed.eesti.ee/api/datasets/";
+
+fetch(baseURL)
+    .then(response => {
+        return response.json();
+    })
+
+    .then( ({ data, metadata })=> {
+        console.log(metadata);
+
+        data.forEach(category => {
+            const optionHTMLElement = new Option(category.name, category.id);
+            selectHTMLElement.appendChild(optionHTMLElement);
+            console.log(category);
+        });
+
+    })
+
+    .catch(error => {
+        console.error('Error fetching data: ', error);
+    });
+
+   
